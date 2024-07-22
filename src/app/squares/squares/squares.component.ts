d746b115d308db891bdf549ef9bb43f86823a8f3
lamont <knahlej@gmail.com>
2024-07-21 22:58:05 -0500

    testing testing

---
+++ src/app/squares/squares/squares.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-squares',
+  standalone: true,
+  imports: [],
+  templateUrl: './squares.component.html',
+  styleUrl: './squares.component.scss'
+})
+export class SquaresComponent {
+
+}
